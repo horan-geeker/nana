d604e56551717f0405cb9456c86f7b620dac258b
贺钧威 <13571899655@163.com>
2019-08-24 18:19:15 +0800

    fix: email notice

--- services/email_service.lua
+++ services/email_service.lua
@@ -5,12 +5,23 @@ local cjson = require('cjson')
 local _M = {}
 
 function _M:notify_comment(to, author, commenter, content, post_id)
-    local mail_content = urlencode("hi " .. author .. "\n    " .. content .. "\n" .. commenter .. "\n\n点击链接跳转相应文章：" .. config.web_url .. "/posts/" .. post_id)
+    local mail_content = "hi " .. author .. "<br><br>" .. content .. "<br><br>" .. commenter .. "<br><br><a href=\"" .. config.web_url .. "/posts/" .. post_id .. "\">点此链接跳转相应文章</a>"
+    local params = {
+        email = to,
+        content = urlencode(mail_content),
+        title = urlencode('Lua 中国 - 新评论通知'),
+        from = urlencode('Lua 中国')
+    }
+    local query = '?'
+    for k,v in pairs(params) do
+        query = query .. k .. '=' .. v .. "&"
+    end
     ngx.timer.at(0,
-            function (premature, to, author, commenter, mail_content, post_id)
+            function (premature, to, author, commenter, query, post_id)
                 local httpc = http.new()
                 -- urlencode
-                local response, err = httpc:request_uri(config.notify_service_url .. '/send/mail?email=' .. to .. '&content=' .. mail_content .. '&title=' .. 'Lua 中国 - 新评论通知' .. '&from=' .. 'Lua 中国')
+                local url = config.notify_service_url .. '/send/mail' .. query
+                local response, err = httpc:request_uri(url)
                 if err ~= nil then
                     ngx.log(ngx.ERR, err)
                     return
@@ -20,7 +31,7 @@ function _M:notify_comment(to, author, commenter, content, post_id)
                     ngx.log(ngx.ERR, response.body)
                 end
             end,
-        to, author, commenter, content, post_id)
+        to, author, commenter, query, post_id)
 end
 
 return _M
